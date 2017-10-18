2733a0abbfa6b61bd28cc17ba45e77403e838ba1
johannadevos <j.devos@donders.ru.nl>
2017-10-18 17:48:45 +0200

    Added English AIP data and made a start with reading and preprocessing the data.

--- automaticGrading.py
+++ automaticGrading.py
@@ -1,4 +1,32 @@
 # -*- coding: utf-8 -*-
 
-print("hello world")
+# Script written by Johanna de Vos, U908153
+# Text and multimedia mining
+# Automatic grading of open exam questions
 
+# Import modules
+import os
+import pandas
+
+# Set working directory
+os.chdir('C:/Users/U908153/Desktop/Github/AutomaticGrading')
+
+# Open file
+def open_file():
+    with open ('AIP_EN.txt') as file:
+        raw_text = file.read()
+        return raw_text
+
+# Remove irregular white lines
+def remove_white(raw_text):
+    text = raw_text.replace("\n\nTentamennummer", "\nTentamennummer")
+    return text
+
+# Create dataframe
+#def create_df(text):
+    
+
+# Run code
+if __name__ == "__main__":
+    raw_text = open_file()
+    text = remove_white(raw_text)
