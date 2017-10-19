765dcf9f6c7adbd06e3f287e2b6cee6fb57f188e
johannadevos <j.devos@donders.ru.nl>
2017-10-19 13:03:57 +0200

    Created Pandas dataframe, added tokenization and lemmatization. Made a few corrections to the data file.

--- automaticGrading.py
+++ automaticGrading.py
@@ -6,7 +6,11 @@
 
 # Import modules
 import os
-import pandas
+import pandas as pd
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+#from nltk import word_tokenize, sent_tokenize 
 
 # Set working directory
 os.chdir('C:/Users/U908153/Desktop/Github/AutomaticGrading')
@@ -15,18 +19,114 @@ os.chdir('C:/Users/U908153/Desktop/Github/AutomaticGrading')
 def open_file():
     with open ('AIP_EN.txt') as file:
         raw_text = file.read()
+        
         return raw_text
 
-# Remove irregular white lines
-def remove_white(raw_text):
-    text = raw_text.replace("\n\nTentamennummer", "\nTentamennummer")
+# Preprocess text
+def preprocess(raw_text):
+    
+    # For empty anwers, insert a dash
+    text = raw_text.replace("Antwoord:\n", "Antwoord: -\n")
+        
+    # Remove white space
+    text = text.replace("\\n", "")
+    text = text.replace(" /n ", "")
+    text = text.replace("/n", "")
+    text = text.replace("  ", " ")
+    text = text.replace("\n\nTentamennummer", "\nTentamennummer")
+    
+    # Replace curly quotes
+    text = text.replace(chr(0x2019), chr(0x0027)) # Replace right single curly quote with straight quote
+    text = text.replace(chr(0x2018), chr(0x0027)) # Replace left single curly quote with straight quote
+    text = text.replace(chr(0x201D), chr(0x0022)) # Replace right double curly quotes with straight quotes
+    text = text.replace(chr(0x201C), chr(0x0022)) # Replace left double curly quotes with straight quotes
+
+    # Replace abbreviated verbs
+    text = text.replace("can't", "cannot")
+    text = text.replace("n't", " not")
+    
+    # Split text by newline
+    text = text.split("\n")
+    
     return text
 
-# Create dataframe
-#def create_df(text):
+# Rearrange the data in a dataframe
+def create_df(text):
+    exam_numbers = []
+    subject_codes = []
+    grades = []
+    answers = []
     
+    # Extract information from running text
+    for i in range(len(text)):
+        if i%4 == 0:
+            exam_number = text[i][16:]
+            exam_numbers.append(exam_number)
+        elif i%4 == 1:
+            subject_code = text[i][13:]
+            subject_codes.append(subject_code)
+        elif i%4 == 2:
+            grade = text[i][7:]
+            grades.append(grade)
+        elif i%4 == 3:
+            answer = text[i][10:]
+            answers.append(answer)
+            
+    # Create dataframe
+    df = pd.DataFrame({'Exam number': exam_numbers, 'Subject code': subject_codes, 'Grade': grades, 'Answer': answers})       
+    
+    # Add empty columns that can later contain tokenized and lemmatized data
+    df['Tokenized'] = ""
+    df['Lemmatized'] = ""
+    
+    # Change order of columns
+    cols = ['Subject code', 'Exam number', 'Grade', 'Answer', 'Tokenized', 'Lemmatized']
+    df = df[cols]    
+
+    return df
+
+# Tokenize and lemmatize
+def tok_lem(df):
+    
+    # Set up tokenizer and lemmatizer
+    tokenizer = RegexpTokenizer(r'\w+')
+    lemmatizer = WordNetLemmatizer()    
+    
+    for i in range(len(df)):
+        answer = df['Answer'][i]
+        
+        # Preprocess
+        answer = answer.replace("'s", "") # Remove possessive 's
+        answer = answer.lower()
+                               
+        # Tokenize
+        #tok_answer = word_tokenize(answer)
+        tok_answer = tokenizer.tokenize(answer) # also removes apostrophe
+        df['Tokenized'][i] = tok_answer
+        
+        # Lemmatize
+        lem_answer = []
+        
+        for word in tok_answer:
+            lemma = lemmatizer.lemmatize(word, pos = 'v') # default POS is 'n'
+            
+            # Hand-crafted rules for words not handled well by lemmatizer
+            if lemma.startswith("whorf"):
+                lemma = "whorf"
+            
+            lem_answer.append(lemma)
+        
+        df['Lemmatized'][i] = lem_answer
+
+    return df
+
+
 
 # Run code
 if __name__ == "__main__":
     raw_text = open_file()
-    text = remove_white(raw_text)
+    text = preprocess(raw_text)
+    df = create_df(text)
+    df = tok_lem(df)
+
+
